81217885d7984182ba2db015f95ff083ce5bd877
Hans_Tausend <einund.tausend@gmail.com>
2019-01-13 16:28:39 +0100

    serve changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const con = mongoose.createConnection('mongodb://localhost/postdb');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 const offers = require('./routes/api/offers');
 
