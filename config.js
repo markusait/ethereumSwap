b39a496717c0d8b5739a574896edfa236cb288db
Hans_Tausend <einund.tausend@gmail.com>
2019-01-16 22:40:48 +0100

    added ropsten deplyoment (656154000000000 wei needed)

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 //gitignore!!!!
 module.exports = {
-  mongoURI: "mongodb://Hanstausend:Omega1908@ds149894.mlab.com:49894/ethereum_bridge"  
+  mongoURI: "mongodb://Hanstausend:Omega1908@ds149894.mlab.com:49894/ethereum_bridge"
+  mnemonic: "merry quick because visa early stone venture ring image fuel radar repeat hour dish approve"
+
 }
--- truffle.js
+++ truffle.js
@@ -1,13 +1,21 @@
 const path = require("path");
+let HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic = require('./config.js').mnemonic;
 
 module.exports = {
 
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,  //using local ganach blockchain
+      port: 7545, //using local ganach blockchain
       network_id: "*", // Match any network id
-      from:"0xe87f722A65c55b7b625884c61d3B95030B6bef27"
+      from: "0xe87f722A65c55b7b625884c61d3B95030B6bef27"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/3cad50878c924727bcc2cc0fb99cf3960")
+      },
+      network_id: 3
     }
   },
   compilers: {
